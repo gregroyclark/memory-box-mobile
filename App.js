bdde0f18507c7ac27dfa1b48dc452447204a5703
gregroyclark <gregroyclark@gmail.com>
2021-05-21 23:27:58 -0500

    added firebaseConfig

--- App.js
+++ App.js
@@ -7,6 +7,15 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import Landing from './Components/auth/Landing'
 
+const firebaseConfig = {
+  apiKey: process.env.RN_FIREBASE_API_KEY,
+  authDomain: process.env.RN_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.RN_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.RN_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.RN_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.RN_FIREBASE_APP_ID
+};
+
 const Stack = createStackNavigator();
 export default function App() {
   return (
