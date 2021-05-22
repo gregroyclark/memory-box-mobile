09b7818061d3f368ca7ebf6610a94683209b6638
gregroyclark <gregroyclark@gmail.com>
2021-05-22 09:48:53 -0500

    changed App to class component

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 
 import * as firebase from 'firebase';
 import { NavigationContainer } from '@react-navigation/native';
@@ -9,12 +9,12 @@ import Landing from './Components/auth/Landing'
 import Register from './Components/auth/Register'
 
 const firebaseConfig = {
-  apiKey: process.env.RN_FIREBASE_API_KEY,
-  authDomain: process.env.RN_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.RN_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.RN_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.RN_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.RN_FIREBASE_APP_ID
+  apiKey: "AIzaSyDGwv7VxatRiwK0GdxNh-ycV6H4czdGjJc",
+  authDomain: "memory-box-mobile.firebaseapp.com",
+  projectId: "memory-box-mobile",
+  storageBucket: "memory-box-mobile.appspot.com",
+  messagingSenderId: "1038231219439",
+  appId: "1:1038231219439:web:da07ab362d57edc3b8ff1a"
 };
 
 if (firebase.apps.length === 0) {
@@ -22,15 +22,28 @@ if (firebase.apps.length === 0) {
 }
 
 const Stack = createStackNavigator();
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Landing">
-        <Stack.Screen name="Landing" component={Landing} options={{ headerShown: false}}/>
-        <Stack.Screen name="Register" component={Register}/>
-      </Stack.Navigator>
-    </NavigationContainer>
 
-  ); 
+import React, { Component } from 'react'
+
+export class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      loaded: false,
+    }
+  }
+    
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Landing">
+          <Stack.Screen name="Landing" component={Landing} options={{ headerShown: false}}/>
+          <Stack.Screen name="Register" component={Register}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+
+    ); 
+  }
 }
 
+export default App
