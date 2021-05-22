53c695116a70ce0c76b1d3a1fd8683cc8cc12143
gregroyclark <gregroyclark@gmail.com>
2021-05-21 22:04:53 -0500

    package.json: fixed expo error; Components/auth: added Landing & Register; App.js:  added routes

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Landing from './Components/auth/Landing'
+
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>I am building a mobile app with React Native.</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Landing">
+        <Stack.Screen name="Landing" component={Landing} options={{ headerShown: false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+
+  ); 
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
